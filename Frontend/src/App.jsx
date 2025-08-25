a109cb099807dab88ba0c793d82bd81f421835e8
ARNAVSAROJ <178799881+ArnavSaroj@users.noreply.github.com.>
2025-08-25 20:44:49 +0530

    changes in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout.jsx";
+import SignUpPage from "./pages/Signup.jsx";
+
 function App() {
   return (
     <Router>
-      {" "}
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <Layout />
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/search-flights" element={<Layout />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
       </div>
     </Router>
   );
