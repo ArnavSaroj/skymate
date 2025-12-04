b604712874a03f96021422d774ca62b5b755bd53
ARNAVSAROJ <178799881+ArnavSaroj@users.noreply.github.com.>
2025-12-04 19:13:55 +0530

    some changes

--- Frontend/src/pages/SearchResults.jsx
+++ Frontend/src/pages/SearchResults.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import FlightResultsTable from "../components/search-result/searchResultTable.jsx";
+import FlightResultsTable from "../components/search-result/SearchResultTable.jsx";
 import { SearchApi } from "../routes/searchApi.js";
-import PricesTrends from "../components/charts/pricesTrends.jsx";
+import PricesTrends from "../components/charts/PricesTrends.jsx";
 import historyPrices from "../routes/historyPrices.js";
 
 
